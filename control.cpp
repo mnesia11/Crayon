9060d1599f6065b28514068f03e07746e5af6db2
optimize-2 <2086297211@qq.com>
2020-01-23 21:24:37 +0800

    a

--- control.cpp
+++ control.cpp
@@ -6,15 +6,5 @@ using namespace std;
 
 int main() {
 	
-	for(int data=1;data<=10;data++) {
-		make_in(data);
-		int n=crayon_min(1000,crayon_max(0,(1000/200)*data*data*2+cyrand(-10,10)));
-		int m=crayon_min(50000,crayon_max(0,(50000/10000)*data*data*data*10+cyrand(-10,10)));
-		graph<int>g=connect_graph(n/2,m/2,1,cyrand(1,pow(2,data)+10),cyrand)+connect_graph(n/2,m/2,1,cyrand(1,pow(2,data)+10),cyrand);
-		cout<<g;
-		end_in();
-		make_out(data);
-	}
-	
 	return 0;
 }
